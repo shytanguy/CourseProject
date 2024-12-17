f18bc84ca6d6bbbb3a93ea9cf7b77c9aa8906f6d
Andrey <shimpguy1@gmail.com>
2024-12-17 16:27:02 +0300

    .

--- WEBPROGM/obj/Debug/net7.0/BACK.AssemblyInfo.cs
+++ WEBPROGM/obj/Debug/net7.0/BACK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b4dffb231144db4044fc8854d1c4ec5ead6c1a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
